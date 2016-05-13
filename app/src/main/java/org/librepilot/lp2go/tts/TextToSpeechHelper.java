cdcbf1ebd740c33c8fa0bac24078d1fcc9b9946a
Marcus Proest <proemarc@gmail.com>
2016-05-13 21:14:48 +0200

    tts foundation & "armed" tts

--- app/src/main/java/org/librepilot/lp2go/tts/TextToSpeechHelper.java
+++ app/src/main/java/org/librepilot/lp2go/tts/TextToSpeechHelper.java
@@ -1,3 +1,18 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package org.librepilot.lp2go.tts;
 
 import android.content.Intent;
@@ -8,9 +23,6 @@ import org.librepilot.lp2go.VisualLog;
 
 import java.util.Locale;
 
-/**
- * Created by Marcus on 13.05.2016.
- */
 public class TextToSpeechHelper implements TextToSpeech.OnInitListener {
 
     private MainActivity mActivity;
@@ -29,14 +41,14 @@ public class TextToSpeechHelper implements TextToSpeech.OnInitListener {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == MainActivity.CALLBACK_TTS) {
             if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-                // success, create the TTS instance
                 mTts = new TextToSpeech(mActivity, this);
             } else {
-                // missing data, install it
-                Intent installIntent = new Intent();
-                installIntent.setAction(
-                        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-                mActivity.startActivity(installIntent);
+                //Intent installIntent = new Intent();
+                //installIntent.setAction(
+                //        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                //mActivity.startActivity(installIntent);
+
+                mTts = null;
             }
         }
     }
