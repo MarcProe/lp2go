c3ee26e798ef7e19fb8a83d7653c7e4c16820c0c
Marcus Proest <proemarc@gmail.com>
2016-05-25 22:26:03 +0200

    minor changes to splash class & (c)

--- app/src/main/java/org/librepilot/lp2go/SplashActivity.java
+++ app/src/main/java/org/librepilot/lp2go/SplashActivity.java
@@ -1,3 +1,38 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* This file incorporates work covered by the following copyright and
+ * permission notice:
+ */
+
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.librepilot.lp2go;
 
 import android.app.Activity;
@@ -10,34 +45,25 @@ import android.widget.ImageView;
 
 public class SplashActivity extends Activity {
 
-    /**
-     * Duration of wait
-     **/
-    private final int SPLASH_DISPLAY_LENGTH = 1200;
-
-    /**
-     * Called when the activity is first created.
-     */
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
 
         ImageView imgSplash = (ImageView) findViewById(R.id.imgSplash);
         Animation pulse = AnimationUtils.loadAnimation(this, R.anim.pulse);
         imgSplash.startAnimation(pulse);
 
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
+        final int SPLASH_DISPLAY_LENGTH = 1200;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
-
     }
 }
